8662449c3056e62259bbc51d0728d9cce7375ccb
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-11-12 15:02:34 +0900

    Button Function Connect
    + Connect Function on Setting Button
    + Connect Function on Alice Button

---
+++ Assets/Scripts/Title/SettingButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingButton : MonoBehaviour
+{
+    public GameObject SettingPanel;
+
+    [SerializeField]
+    private bool isPanelOn;
+
+    private void Start()
+    {
+        isPanelOn = false;
+        SettingPanel.SetActive(false);
+    }
+
+    public void ClickSettingButton()
+    {
+        if (!isPanelOn)
+        {
+            SettingPanel.SetActive(true);
+        }
+        else
+        {
+            SettingPanel.SetActive(false);
+        }
+
+        isPanelOn = !isPanelOn;
+    }
+}
